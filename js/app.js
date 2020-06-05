b50e06f40a258bd439b0f5cc5ec844a7c2bb7f1a
AlexCSS <kalyuzhnyi@gmail.com>
2020-06-05 21:54:02 +0300

    ajax

--- js/app.js
+++ js/app.js
@@ -10,3 +10,37 @@ function msToTime(duration) {
 	seconds = (seconds < 10) ? "0" + seconds : seconds;
 	return minutes + ":" + seconds;
 }
+
+const searchForm = document.querySelector('[data-search]');
+
+searchForm.addEventListener('submit', sendRequest);
+
+function sendRequest(event){
+	event.preventDefault();
+	console.log('ajax rocks');
+
+	// let url = "http://itunes.apple.com/search?term=vitas";
+	let url = `${this.action}?term=${this.elements.query.value}`;
+
+	fetch(url)
+		.then( result => result.json() )
+		.then( showTunes );	
+
+	
+}
+const listEl = document.querySelector('[data-list]');
+const tmpl = document.querySelector('[data-tmpl]').innerHTML;
+
+function showTunes(data) {
+	console.log(data);
+
+	listEl.innerHTML = ''; 
+
+	data.results.forEach( tune => {
+		listEl.innerHTML += tmpl
+						.replace(/{{audio}}/gi, tune.previewUrl)
+						.replace(/{{title}}/gi, tune.trackName)
+						.replace(/{{img}}/gi, tune.artworkUrl100)
+					;
+	})
+}
